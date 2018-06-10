9fc7a245c2fb40cda5f4c241e4fbac208c77c110
nayan2823 <nayan2823@gmail.com>
2018-06-10 23:53:29 +0530

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// second commit just for git
+
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
